c36c4199aabac624bb8922993a9738e658f4e648
Oleg Ilin <ilyinon@mail.ru>
2024-11-17 03:23:22 +0300

    add_sentry

--- ugc2/app/main.py
+++ ugc2/app/main.py
@@ -1,7 +1,21 @@
+import sentry_sdk
 from api.v1 import films, users
 from core.config import ugc2_settings
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+
+sentry_sdk.init(
+    dsn="https://c6e15651de424b3321b89771c9ec00bb@o4508310740598784.ingest.de.sentry.io/4508310743941200",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },
+)
 
 app = FastAPI(
     title=ugc2_settings.project_name,
